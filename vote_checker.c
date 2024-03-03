2ac839b0df4a7c9fcb1da1d0fb42121d0d86eee3
Shankar Vallabhan A <shankarmanu@protonmail.com>
2024-03-03 20:26:23 +0530

    Add a c program to check age

---
+++ vote_checker.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main () 
+{ int age;
+  
+	printf("Enter your age :");
+	scanf("%d" , &age);
+
+	if( age  >= 18 ) printf("Eligible to vote \n";
+	if( age < 18 ) printf("Not Eligible to vote \n");
+
+	return 0;
+
+}
+
+
+
